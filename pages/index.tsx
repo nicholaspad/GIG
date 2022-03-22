92deec0db7cf5c94b1dc625a665dc4466744ef6d
Nicholas Padmanabhan <13815069+nicholaspad@users.noreply.github.com>
2022-03-22 16:22:28 -0400

    CTA Buttons (#3)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,6 @@
 import { Box, ThemeProvider, Typography } from "@mui/material";
+import PrimaryButtonCTA from "../components/buttons/PrimaryButtonCTA";
+import SecondaryButtonCTA from "../components/buttons/SecondaryButtonCTA";
 import { gigTheme } from "../src/Theme";
 
 export default function Landing() {
@@ -16,6 +18,13 @@ export default function Landing() {
         >
           Secondary color text
         </Typography>
+        <PrimaryButtonCTA text="Big Primary CTA" size="big" to="/" />
+        <br />
+        <PrimaryButtonCTA text="Small Primary CTA" size="small" to="/" />
+        <br />
+        <SecondaryButtonCTA text="Big Secondary CTA" size="big" to="/" />
+        <br />
+        <SecondaryButtonCTA text="Small Secondary CTA" size="small" to="/" />
       </Box>
     </ThemeProvider>
   );
