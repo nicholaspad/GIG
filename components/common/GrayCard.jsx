66747571d75ec24ccce642b2184b56dbb1daa80f
Zishuo Zhang <zishuoz@princeton.edu>
2022-03-24 23:27:34 -0400

    added gray card template

---
+++ components/common/GrayCard.jsx
@@ -0,0 +1,22 @@
+import Card from "@mui/material";
+import shadows from "../../styles/shadows";
+
+export default function GrayCard(props) {
+  return (
+    <Card
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "flex-start",
+        py: "3%",
+        px: "5%",
+        my: "3%",
+        bgcolor: "background.paper",
+        borderRadius: 1,
+        boxShadow: shadows[8],
+      }}
+    >
+      {props.children}
+    </Card>
+  );
+}
