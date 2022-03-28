c0288691387bc0c648374cdf810f5a84b1960cc2
Nicholas Padmanabhan <nicholaspad@gmail.com>
2022-03-28 00:52:34 -0400

    Create landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,81 @@
-import { Box, Typography } from "@mui/material";
-import PrimaryButtonCTA from "../components/buttons/PrimaryButtonCTA";
-import SecondaryButtonCTA from "../components/buttons/SecondaryButtonCTA";
+import { Box, Container, Typography } from "@mui/material";
+import { gigTheme } from "../src/Theme";
 
 export default function Landing() {
   return (
-    <Box sx={{ p: 3 }}>
-      <Typography variant="h4" fontStyle="bold" color="primary.main">
-        Primary color text
-      </Typography>
-      <Typography variant="h5" fontStyle="italic" mt={2} color="secondary.main">
-        Secondary color text
-      </Typography>
-      <PrimaryButtonCTA text="Tasks" size="big" to="/tasks" />
-      <br />
-      <PrimaryButtonCTA text="My Tasks" size="small" to="/tasker/mytasks" />
-      <br />
-      <SecondaryButtonCTA text="Big Secondary CTA" size="big" to="/" />
-      <br />
-      <SecondaryButtonCTA text="Small Secondary CTA" size="small" to="/" />
-    </Box>
+    <Container maxWidth="md">
+      <Box display="flex" flexDirection="column" alignItems="center">
+        <Typography
+          fontWeight={700}
+          color="primary"
+          height="fit-content"
+          component="div"
+          sx={{
+            fontSize: {
+              lg: 250,
+              md: 200,
+              sm: 150,
+              xs: 125,
+            },
+            backgroundImage: `linear-gradient(90deg, ${gigTheme.palette.primaryCTA.primary}, ${gigTheme.palette.primaryCTA.secondary})`,
+            backgroundClip: "text",
+            WebkitTextFillColor: "transparent",
+          }}
+        >
+          G I G
+        </Typography>
+        <Typography
+          color="primary"
+          textAlign="center"
+          fontWeight={500}
+          sx={{
+            fontSize: {
+              lg: 28,
+              md: 26,
+              sm: 24,
+              xs: 22,
+            },
+          }}
+        >
+          Take surveys. Earn crypto. The easiest way to get into web3.
+        </Typography>
+        <Typography
+          color="secondary"
+          textAlign="center"
+          fontWeight={500}
+          my={4}
+          sx={{
+            fontSize: {
+              lg: 28,
+              md: 26,
+              sm: 24,
+              xs: 22,
+            },
+          }}
+        >
+          Connect your wallet to get started.
+        </Typography>
+        <Box
+          component="img"
+          src="/metamask-logo.png"
+          borderRadius={3}
+          sx={{
+            background: gigTheme.palette.primary.main,
+            cursor: "pointer",
+            transitionDuration: "0.2s",
+            "&:hover": {
+              transform: "scale(1.05)",
+            },
+            width: {
+              lg: 280,
+              md: 250,
+              xs: 230,
+            },
+          }}
+          // TODO @nicholaspad replace with Moralis authentication and remove Link
+          onClick={() => {}}
+        />
+      </Box>
+    </Container>
   );
 }
