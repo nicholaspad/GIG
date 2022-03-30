3a4f7a6a8d0eebe0c57a896d8058c9c6e321e6f9
Nicholas Padmanabhan <nicholaspad@gmail.com>
2022-03-30 14:15:59 -0400

    Add/test logout functionality

--- pages/tasks.tsx
+++ pages/tasks.tsx
@@ -1,8 +1,14 @@
+import { Button } from "@mui/material";
+import router from "next/router";
+import { useEffect } from "react";
+import { useMoralis } from "react-moralis";
 import PageTitle from "../components/common/PageTitle";
 import BrowseTasksTable from "../components/tables/BrowseTasks";
 import { TaskData } from "../components/tables/TasksTable";
 
 export default function Tasks() {
+  const { logout, user, isUnauthenticated } = useMoralis();
+
   // TODO @nicholaspad hard-coded for now
   const data: TaskData[] = [1.5, 2.5, 3, 4.5, 5, 6.5, 7, 8.5, 9, 10, 10.5].map(
     (e, i) => {
@@ -19,6 +25,17 @@ export default function Tasks() {
     <>
       <PageTitle title={"Browse Tasks"} />
       <BrowseTasksTable data={data} />
+      {/* TODO @nicholaspad remove this temporary logout button */}
+      <Button
+        color="secondary"
+        variant="contained"
+        onClick={() => {
+          logout();
+        }}
+        sx={{ mt: 4 }}
+      >
+        Logout from address {user?.get("ethAddress")}
+      </Button>
     </>
   );
 }
