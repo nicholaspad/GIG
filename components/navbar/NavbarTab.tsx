2f0233adbfa5a54d151fd9c046921837eace2b52
Nicholas Padmanabhan <nicholaspad@gmail.com>
2022-04-01 16:35:42 -0400

    Add link functionality for tasker/requester switch

--- components/navbar/NavbarTab.tsx
+++ components/navbar/NavbarTab.tsx
@@ -1,5 +1,5 @@
 import { Box, Typography } from "@mui/material";
-import * as React from "react";
+import Link from "next/link";
 import { gigTheme } from "../../src/Theme";
 
 export default function NavbarTab(props: {
@@ -10,26 +10,29 @@ export default function NavbarTab(props: {
   tabName: string;
 }) {
   return (
-    <Box
-      sx={{
-        background: props.isSelected ? gigTheme.palette.secondary.main : null,
-        p: 0.5,
-        borderRadius: props.isSelected ? 1.5 : null,
-      }}
-    >
-      <Typography
-        color="primary"
-        fontWeight={props.isSelected ? 800 : 400}
+    // TODO @nicholaspad replace second link with requester table route
+    <Link href={props.tabIsTasker ? "/tasker/my-tasks" : ""}>
+      <Box
         sx={{
-          "&:hover": {
-            cursor: "pointer",
-            transition: "ease 0.1s",
-          },
+          background: props.isSelected ? gigTheme.palette.secondary.main : null,
+          p: 0.5,
+          borderRadius: props.isSelected ? 1.5 : null,
         }}
-        onClick={() => props.setCurrentTask(props.tabIsTasker)}
       >
-        {props.tabName}
-      </Typography>
-    </Box>
+        <Typography
+          color="primary"
+          fontWeight={props.isSelected ? 800 : 400}
+          sx={{
+            "&:hover": {
+              cursor: "pointer",
+              transition: "ease 0.1s",
+            },
+          }}
+          onClick={() => props.setCurrentTask(props.tabIsTasker)}
+        >
+          {props.tabName}
+        </Typography>
+      </Box>
+    </Link>
   );
 }
