59a6c2f5ae63caca1546965f534322536ffea2a6
Nicholas Padmanabhan <nicholaspad@gmail.com>
2022-04-04 20:22:34 -0400

    Move TaskStatus and TaskData back to original file

--- src/Types.ts
+++ src/Types.ts
@@ -1,24 +1,14 @@
-export type TaskStatus = 0 | 1 | 2 | 3;
-
-export type TaskData = {
-    task_id: number;
-    name: string;
-    rating?: number;
-    status?: TaskStatus;
-    reward: number;
+export type mcQuestionProps = {
+  idx: number;
+  id: string;
+  question: string;
+  options: string[];
 };
 
-export type mcQuestionProps = {
-    idx: number;
-    id: string;
-    question: string;
-    options: string[];
-}
-
 export type questionProps = {
-    type: number;
-    idx: number;
-    id: string;
-    question: string;
-    options?: string[];
-}
+  type: number;
+  idx: number;
+  id: string;
+  question: string;
+  options?: string[];
+};
