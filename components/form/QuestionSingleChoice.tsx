f96ab263e03afebb3a38aa3598b9b058c65e265c
Nicholas Padmanabhan <nicholaspad@gmail.com>
2022-04-04 20:22:38 -0400

    Format code with Prettier

--- components/form/QuestionSingleChoice.tsx
+++ components/form/QuestionSingleChoice.tsx
@@ -1,80 +1,74 @@
-import React, { useState, useRef } from 'react';
-import TextField from '@mui/material/TextField';
-import FormControl from '@mui/material/FormControl';
-import Radio from '@mui/material/Radio';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import RadioGroup from '@mui/material/RadioGroup';
-import DefaultGrayCard from '../common/DefaultGrayCard'
-import Button from '@mui/material/Button';
+import React, { useState, useRef } from "react";
+import TextField from "@mui/material/TextField";
+import FormControl from "@mui/material/FormControl";
+import Radio from "@mui/material/Radio";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import RadioGroup from "@mui/material/RadioGroup";
+import DefaultGrayCard from "../common/DefaultGrayCard";
+import Button from "@mui/material/Button";
 import { mcQuestionProps } from "../../src/Types";
 
-export default function QuestionSingleChoice(
-    props: {
-        index: number
-        removeQuestion: Function
-    }
-) {
+export default function QuestionSingleChoice(props: {
+  index: number;
+  removeQuestion: Function;
+}) {
+  const newOptionRef = useRef(null);
+  const [options, setOptions] = useState([]);
+  const [text, setText] = useState("");
 
-    const newOptionRef = useRef(null);
-    const [options, setOptions] = useState([])
-    const [text, setText] = useState("")
-
-    return (
-        <DefaultGrayCard>
-            <TextField
-                label="Type question here"
-                fullWidth
-                sx={{
-                    m: 1
-                }}
+  return (
+    <DefaultGrayCard>
+      <TextField
+        label="Type question here"
+        fullWidth
+        sx={{
+          m: 1,
+        }}
+      />
+      <FormControl>
+        {/* The Options */}
+        <RadioGroup
+          aria-labelledby="demo-radio-buttons-group-label"
+          name="radio-buttons-group"
+        >
+          {options.map((optionName) => (
+            <FormControlLabel
+              value={optionName}
+              control={<Radio />}
+              label={optionName}
             />
-            <FormControl>
-                {/* The Options */}
-                <RadioGroup
-                    aria-labelledby="demo-radio-buttons-group-label"
-                    name="radio-buttons-group"
-                >
-                    {
-                        options.map((optionName) => (
-                            <FormControlLabel
-                                value={optionName}
-                                control={<Radio/>}
-                                label={optionName}
-                            />
-                        ))
-                    }
-                </RadioGroup>
+          ))}
+        </RadioGroup>
 
-                    {/* Add an option */}
-                    <TextField
-                        label="Add an option..."
-                        ref={newOptionRef}
-                        value={text}
-                        onChange={e => {
-                            const { value } = e.target;
-                            setText(value)
-                        }}
-                        onBlur={e => {
-                            const { value } = e.target;
-                            if (value.trim() !== "") {
-                                setOptions([...options, value])
-                                setText("")
-                            }
-                        }}
-                    />
-            </FormControl>
+        {/* Add an option */}
+        <TextField
+          label="Add an option..."
+          ref={newOptionRef}
+          value={text}
+          onChange={(e) => {
+            const { value } = e.target;
+            setText(value);
+          }}
+          onBlur={(e) => {
+            const { value } = e.target;
+            if (value.trim() !== "") {
+              setOptions([...options, value]);
+              setText("");
+            }
+          }}
+        />
+      </FormControl>
 
-            {/* Delete this question */}
-            <Button
-                color="secondary"
-                onClick={() => {
-                    props.removeQuestion(props.index)
-                }
-                }
-            >
-                Delete
-                {props.index}</Button>
-
-        </DefaultGrayCard>
-    )
+      {/* Delete this question */}
+      <Button
+        color="secondary"
+        onClick={() => {
+          props.removeQuestion(props.index);
+        }}
+      >
+        Delete
+        {props.index}
+      </Button>
+    </DefaultGrayCard>
+  );
 }
--- components/form/TaskHeading.tsx
+++ components/form/TaskHeading.tsx
@@ -1,74 +1,63 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import styles from '../styles/utils.module.css';
-import InputLabel from '@mui/material/InputLabel';
-import OutlinedInput from '@mui/material/OutlinedInput';
-import FormControl from '@mui/material/FormControl';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import FilledInput from '@mui/material/FilledInput';
-import Grid from '@mui/material/Grid';
-import { gigTheme } from '../../src/Theme';
-import Typography from '@mui/material/Typography';
-import DefaultGrayCard from "../common/DefaultGrayCard"
+import * as React from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import styles from "../styles/utils.module.css";
+import InputLabel from "@mui/material/InputLabel";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import FormControl from "@mui/material/FormControl";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FilledInput from "@mui/material/FilledInput";
+import Grid from "@mui/material/Grid";
+import { gigTheme } from "../../src/Theme";
+import Typography from "@mui/material/Typography";
+import DefaultGrayCard from "../common/DefaultGrayCard";
 
 //BELOW IS TEMP
 import { ThemeProvider } from "@mui/material";
 
-
 export default function TaskHeading() {
-    return (
-        <DefaultGrayCard>
-            {/* Task Title */}
-            {/* <ThemeProvider theme={gigTheme}> */}
-            <TextField
-                label="Task Title"
-                variant="filled"
-                fullWidth
-                color="secondary"
-                font={gigTheme.typography.fontFamily}
-                sx={{
-                    m: 1,
-                    color: 'red'
-                }}
-            />
+  return (
+    <DefaultGrayCard>
+      {/* Task Title */}
+      {/* <ThemeProvider theme={gigTheme}> */}
+      <TextField
+        label="Task Title"
+        variant="filled"
+        fullWidth
+        color="secondary"
+        font={gigTheme.typography.fontFamily}
+        sx={{
+          m: 1,
+          color: "red",
+        }}
+      />
 
-            {/* </ThemeProvider> */}
+      {/* </ThemeProvider> */}
 
+      {/* Task Description */}
+      <TextField
+        label="Task Description. (Ex: Answer a survey about your opinions)"
+        variant="filled"
+        fullWidth
+        sx={{ m: 1 }}
+      />
 
-            {/* Task Description */}
-            <TextField
-                label="Task Description. (Ex: Answer a survey about your opinions)"
-                variant="filled"
-                fullWidth
-                sx={{ m: 1 }}
-            />
-
-            {/* Crypto Allocation */}
-            <Grid container spacing={2}>
-                <Grid item xs={8}>
-                    <Box
-                        display="flex"
-                        alignItems="center"
-                        justifyContent="left"
-                    >
-                        Total crypto allocated:
-                        <TextField sx={{ m: 0.5 }}></TextField>
-                        ETH
-                    </Box>
-                </Grid>
-                <Grid item xs={4}>
-                    <Box
-                        display="flex"
-                        alignItems="center"
-                        justifyContent="right"
-                    >
-                        Number of Taskers
-                        <TextField sx={{ m: 0.5 }}></TextField>
-                    </Box>
-                </Grid>
-            </Grid>
-
-        </DefaultGrayCard>
-    )
+      {/* Crypto Allocation */}
+      <Grid container spacing={2}>
+        <Grid item xs={8}>
+          <Box display="flex" alignItems="center" justifyContent="left">
+            Total crypto allocated:
+            <TextField sx={{ m: 0.5 }}></TextField>
+            ETH
+          </Box>
+        </Grid>
+        <Grid item xs={4}>
+          <Box display="flex" alignItems="center" justifyContent="right">
+            Number of Taskers
+            <TextField sx={{ m: 0.5 }}></TextField>
+          </Box>
+        </Grid>
+      </Grid>
+    </DefaultGrayCard>
+  );
 }
