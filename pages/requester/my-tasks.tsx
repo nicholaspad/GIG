5c8a5d98d5ad1b45524495a07ec91c51ebb7fd46
Nicholas Padmanabhan <nicholaspad@gmail.com>
2022-04-07 23:51:06 -0400

    Hook up DB to requester tasks table

--- pages/requester/my-tasks.tsx
+++ pages/requester/my-tasks.tsx
@@ -1,28 +1,49 @@
+import MoralisType from "moralis";
+import { useEffect, useState } from "react";
+import { useMoralis } from "react-moralis";
 import PageHeader from "../../components/common/PageHeader";
 import MyTasksTable from "../../components/tables/MyTasksTable";
 import {
   CreatedTaskStatus,
   TaskData,
 } from "../../components/tables/TasksTable";
+import { getRequesterCreatedTasksTableData } from "../../src/Database";
 
 export default function MyTasks() {
-  const data: TaskData[] = [1.5, 2.5, 3, 4.5, 5, 6.5, 7, 8.5, 9, 10, 10.5].map(
-    (e, i) => {
-      return {
-        task_id: String(e),
-        name: `Task ${i + 1}`,
-        status: (i % 3) as CreatedTaskStatus,
-        reward: e,
-        completedTasks: Math.floor(Math.random() * 50),
-        totalTasks:
-          Math.floor(Math.random() * 50) + Math.floor(Math.random() * 50),
-      };
-    }
-  );
+  const { isInitialized, Moralis } = useMoralis();
+  const [data, setData] = useState<TaskData[]>([]);
+  const [userData, setUserData] = useState<MoralisType.Object>();
+
+  useEffect(() => {
+    if (!isInitialized || !userData) return;
+
+    getRequesterCreatedTasksTableData(Moralis, userData.get("ethAddress")).then(
+      (res) => {
+        let tempData: TaskData[] = [];
+
+        for (let task_ of res) {
+          let task = task_ as any;
+          tempData.push({
+            task_id: task["objectId"],
+            name: task["title"],
+            reward: task["unitReward"],
+            maxReward: task["maxReward"],
+            numResponses: task["numResponses"],
+            maxResponses: task["maxResponses"],
+            status: task["status"] as CreatedTaskStatus,
+          });
+        }
+        setData(tempData);
+      }
+    );
+  }, [isInitialized, userData]);
 
   return (
     <>
-      <PageHeader title="Requester Created Tasks" />
+      <PageHeader
+        title="Requester Created Tasks"
+        customSetUserData={setUserData}
+      />
       <MyTasksTable type={2} data={data} />
     </>
   );
